f40ea24f1d4761adc8d9ecc942ed1f869f77a3e1
big2xa <awexome@me.com>
2015-09-01 19:40:17 -0400

    Initial Commit for me. Game in place (with high/lower). About to start Eleven Note

---
+++ ElevenNote.web/Controllers/GameController.cs
@@ -0,0 +1,54 @@
+﻿using ElevenNote.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.web.Controllers
+{
+    public class GameController : Controller
+    { 
+        #region GET 
+        // GET: Game
+        [HttpGet]
+        [ActionNameAttribute("Index")]
+        public ActionResult IndexGet()
+        {
+            var correctAnswer = new Random().Next(1, 100);
+            Session["Answer"] = correctAnswer;
+            var foo = Session["Answer"];
+            return View();
+        }
+
+        #endregion
+
+        #region POST
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost(GuessingGameViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var answer = (int)Session["Answer"];
+                if (model.Guess == answer)
+                {
+                    ViewBag.Win = true;
+                }
+                else if(model.Guess < answer)
+                {
+                    ViewBag.HighGuess = false;
+                    ViewBag.Win = false;
+                }
+                else if (model.Guess > answer)
+                {
+                    ViewBag.HighGuess = true;
+                    ViewBag.Win = false;
+                }
+            }
+            return View(model);
+        }
+        #endregion
+    }
+}
---
+++ ElevenNote.web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.web.Models
+{
+    public class GuessingGameViewModel
+    {
+        [Required(ErrorMessage = "What's your name, who shot ya?!")]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [Display(Name = "Your Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,100)]
+        [Display(Name ="Your Guess")]
+        public int Guess { get; set; }
+
+    }
+}
